93833c398121fe2dc69d8a84b9c3878b92384e54
RAHAMSHAIK <81603735+RAHAMSHAIK007@users.noreply.github.com>
2024-10-03 16:46:05 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -5,8 +5,8 @@ region = "us-east-1"
 resource "aws_instance" "one" {
   ami           = "ami-04823729c75214919"
   instance_type = "t2.micro"
-  key_name      = "newkp"
-  vpc_security_group_ids = ["sg-0a96cf162f9dd4d2f"]
+  key_name      = "dockerr"
+  vpc_security_group_ids = ["sg-05cfa7da6d7e8def3"]
   tags = {
     Name = " rahaminstance"
   }
