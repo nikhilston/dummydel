d6aadbfa33af8b72a7f97f2c6788bd8ad171f271
RAHAMSHAIK <81603735+RAHAMSHAIK007@users.noreply.github.com>
2024-10-03 16:36:35 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -2,17 +2,12 @@ provider "aws" {
 region = "us-east-1"
 }
 
-terraform {
- backend "remote" {
-    hostname = "app.terraform.io"
-    organization = "default"
-
-    workspaces {
-      name = "dummydel"
-    }
+resource "aws_instance" "one" {
+  ami           = "ami-04823729c75214919"
+  instance_type = "t2.micro"
+  key_name      = "newkp"
+  vpc_security_group_ids = ["sg-0a96cf162f9dd4d2f"]
+  tags = {
+    Name = " rahaminstance"
   }
-}
-
-resource "aws_iam_user" "user_two" {
-  name = "abc-user"
 }
