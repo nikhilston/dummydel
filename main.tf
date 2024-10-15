52f9be6a49159dab71bda68213499269e674c920
Nikhilston <123053913+nikhilston@users.noreply.github.com>
2024-10-15 16:49:04 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -6,7 +6,7 @@ resource "aws_instance" "one" {
   count         = 5
   ami           = "ami-04823729c75214919"
   instance_type = "t2.micro"
-  key_name      = "dockerr"
+  key_name      = "devops29-new"
   vpc_security_group_ids = ["sg-05cfa7da6d7e8def3"]
   tags = {
     Name = "test-server"
