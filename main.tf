fa00df2649ff551acb8a4839f42c47754ebeb294
Nikhilston <123053913+nikhilston@users.noreply.github.com>
2024-10-15 16:57:15 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -4,10 +4,9 @@ region = "us-east-1"
 
 resource "aws_instance" "one" {
   count         = 5
-  ami           = "ami-04823729c75214919"
+  ami           = "ami-00f251754ac5da7f0"
   instance_type = "t2.micro"
   key_name      = "devops29-new"
-  vpc_security_group_ids = ["sg-05cfa7da6d7e8def3"]
   tags = {
     Name = "test-server"
   }
